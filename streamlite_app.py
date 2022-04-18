95ce3cef43f280e993fc222dfecc344672508474
sn0w7lak3ta <103886092+sn0w7lak3ta@users.noreply.github.com>
2022-04-18 17:52:54 +0200

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -25,5 +25,5 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/Watermelon')
-treamlite.text(fruityvice_response.json())
+streamlite.text(fruityvice_response.json())
 
